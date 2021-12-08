ed4013d6f5e6051180b89fd2b2ee574a98179550
daichiterazawa <teratera@opt.comm.eng.osaka-u.ac.jp>
2021-12-08 11:51:05 +0900

    12/8更新

--- AtCoder Beginner Contest 230/C - X drawing.py
+++ AtCoder Beginner Contest 230/C - X drawing.py
@@ -9,10 +9,21 @@ import numpy as np
 N,A,B = map(int,input().split())
 P,Q,R,S = map(int,input().split())
 
-#kがリストの数字なら黒く塗られる
-k1 = np.zeros((Q-P+1,S-R+1))
-k2 = np.zeros((Q-P+1,S-R+1))
-
+for i in range(P,Q+1):
+    for j in range(R,S+1):
+        k = i - A 
+        if k == j - B and max([1-A,1-B]) <= k and min([N-A,N-B]) >= k:
+            print("#",end="")
+            
+        elif k == B - j and max([1-A,B-N]) <= k and min([N-A,B-1]) >= k:
+            print("#",end="")
+            
+        else:
+            print(".",end="")
+    print("\n")
+            
+            
+    
 
 
 
