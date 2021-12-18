cebd70080963323fa0ecaf156a0131c5478569e0
daichiterazawa <teratera@opt.comm.eng.osaka-u.ac.jp>
2021-12-18 11:07:45 +0900

    12/18更新

--- AtCoder Beginner Contest 219/C - Neo-lexicographic Ordering.py
+++ AtCoder Beginner Contest 219/C - Neo-lexicographic Ordering.py
@@ -8,8 +8,13 @@ Created on Fri Dec 17 15:38:07 2021
 X = list(input())
 N = int(input())
 S =[]
-for n in range(N):
-    sn = list(input()) 
-    S.append(sn)
-    
+S_num = []
+ 
+#高橋アルファベットの辞書を作成
+d = {al : i for i,al in enumerate(X)}
 
+#アルファベット番号とアルファベットを2次元配列で格納
+for n in range(N):
+    S = list(input())
+    for m in range(len(S)):
+        
