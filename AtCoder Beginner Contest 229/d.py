6c9a7a6ce4a7c4daff5d99a5817d4b76acaaa1f4
daichiterazawa <teratera@opt.comm.eng.osaka-u.ac.jp>
2022-01-22 19:41:27 +0900

    1/22更新

--- AtCoder Beginner Contest 229/d.py
+++ AtCoder Beginner Contest 229/d.py
@@ -8,6 +8,7 @@ Created on Fri Jan 21 22:24:00 2022
 S = input()
 K = int(input())
 l = 0
+check = 0
 ans = 0
 
 for i,s in enumerate(S):
@@ -16,16 +17,25 @@ for i,s in enumerate(S):
         if s == '.':  
             S[i] = 'O'
             K -= 1
-            ans += 1
+            check += 1
         else:
-            ans += 1
+            check += 1
     
     #置き換えに余裕がない場合    
     else :
         if s == '.':
             #左側をポップ
             if S[l] == 'X':
-                l
+                
+                if ans < check :
+                    ans = check
+                
+                check -= 1 
+                
+            else:
+                l += 1
+                S[i] = 'O'
+                
                 
             
             
